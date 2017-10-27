e1f9b5f80951c2483b3b058e63f35a12cb728044
Ruchita Parmar <rchta26@gmail.com>
2017-10-27 20:05:32 +0530

    Add files via upload

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,18 +1,44 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import csv
 
-html = urlopen("https://www.coursera.org/browse/computer-science/computer-security-and-networks?languages=en")
+def scraper(domain,pageNumber):
+	html = urlopen("https://www.coursera.org/browse/data-science/"+domain+"?languages=en&page="+pageNumber)
 
-soup = BeautifulSoup(html,'lxml')
-soup = soup.find_all("a",attrs={"class": "rc-OfferingCard"})
+	soup = BeautifulSoup(html,'lxml')
+	soup = soup.find_all("a",attrs={"class": "rc-OfferingCard"})
 
-for i in soup:
-    #print(i["href"])
-    html1 = urlopen("https://www.coursera.org" + i["href"])
-    soup1 = BeautifulSoup(html1,'lxml')
-    allCourses = soup1.find_all("h2",attrs={"class":"s12n-headline display-6-text"})
-    allDesc = soup1.find_all("div",attrs={"class":"description subsection"})  
-    
-    for name,desc in zip(allCourses,allDesc):
-        print (name.string + ":" + " \n " + desc.string)
-        print ("\n")
+	title = []
+	description = []
+	courseList = []
+	for i in soup:
+		try:
+			html1 = urlopen("https://www.coursera.org" + i["href"])
+			soup1 = BeautifulSoup(html1,'lxml')
+			allCourses = soup1.find("title")
+			if(i["href"][1]=='s'):
+				allDesc = soup1.find("div",attrs={"class":"description"})
+			else:
+				allDesc = soup1.find("p",attrs={"class":"course-description"})
+				allDesc = list(allDesc)
+				allDesc = allDesc[2]
+			allCourses = (allCourses.string).split('|')[0]
+			allDesc = allDesc.string
+			print(allCourses)
+			print(allDesc)
+			print()
+			print()
+			print()
+			print()
+			x = allCourses.replace(',','~')
+			y = allDesc.replace(',','~')
+			courseList.append([domain,x,y])
+		except Exception as e:
+			print('Oh no: ',e)
+	with open ('strictlyProfessional.csv','a') as file:
+		writer=csv.writer(file)
+		for row in courseList:
+			writer.writerow(row)
+
+for n in range(1,4):
+	scraper(domain='machine-learning',pageNumber=str(n))
