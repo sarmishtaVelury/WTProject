5406acd144ca38a70abb031d8852530e5a5ed949
Ruchita Parmar <rchta26@gmail.com>
2017-10-29 15:21:22 +0530

    Update recombee.py

--- scraping/recombee.py
+++ scraping/recombee.py
@@ -4,18 +4,18 @@ import csv
 
 client = RecombeeClient('wt', '0IHcpVHDwnsQHZBytn9x77SobNPSsepNFbDdC6iSyLmoYLc7CfBK9uAJo37DhNpm')
 
-# purchases = []
+purchases = []
 
-# with open('purchaseRecord.csv') as csvfile:
-# 	csvreader = csv.reader(csvfile)
-# 	for row in csvreader:
-# 		user_id = row[0]
-# 		item_id = row[1]
-# 		r = AddPurchase(user_id, item_id)
-# 		purchases.append(r)
+with open('purchaseRecord.csv') as csvfile:
+	csvreader = csv.reader(csvfile)
+	for row in csvreader:
+		user_id = row[0]
+		item_id = row[1]
+		r = AddPurchase(user_id, item_id)
+		purchases.append(r)
 
-# br = Batch(purchases)
-# client.send(br)
+br = Batch(purchases)
+client.send(br)
 
 recommended = client.send(UserBasedRecommendation('user-0', 5))
 print(recommended)
